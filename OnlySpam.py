f75a1df93cc03a9623b8517648610414652ff67f
Pratik <prratadiya@gmail.com>
2018-07-06 13:49:20 +0530

    [Chores]Pep8

--- OnlySpam.py
+++ OnlySpam.py
@@ -4,39 +4,43 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from imblearn.over_sampling import SMOTE
 import re
 from sklearn.svm import LinearSVC
-import pickle 
-fields = ['Content','Spam']
+import pickle
+fields = ['Content', 'Spam']
 
-df = pd.read_csv('OnlySpam.csv',usecols=fields,skipinitialspace=True)
-#TAG_RE = re.compile(r'<a.*?>.*?</a>')
+df = pd.read_csv('OnlySpam.csv', usecols=fields, skipinitialspace=True)
+# TAG_RE = re.compile(r'<a.*?>.*?</a>')
+
 
 def remove_tags(text):
-#    string = TAG_RE.sub('hyperlink',text)
-    soup = BeautifulSoup(text,"lxml")
+    # string = TAG_RE.sub('hyperlink',text)
+    soup = BeautifulSoup(text, "lxml")
     if soup.find_all('style'):
         soup.style.decompose()
     string = soup.get_text()
-    string = string.replace('&nbsp;','').replace('\n','').replace('\r','').replace('\t','')
-    string = ' '.join([w for w in string.split() if len(w)>=3])
+    string = string.replace('&nbsp;', '').replace(
+        '\n', '').replace('\r', '').replace('\t', '')
+    string = ' '.join([w for w in string.split() if len(w) >= 3])
     return string
 
-df['Content']=df['Content'].apply(remove_tags)
+
+df['Content'] = df['Content'].apply(remove_tags)
 
 vectorizer = TfidfVectorizer(stop_words='english')
 
 x_train = vectorizer.fit_transform(df['Content'])
 
-#sm = SMOTE(random_state=42)
+# sm = SMOTE(random_state=42)
 
-#x_train,y_train = sm.fit_sample(x_train,df['Spam'])
+# x_train,y_train = sm.fit_sample(x_train,df['Spam'])
 
 
 model = LinearSVC()
 
-model.fit(x_train,df['Spam'])
+model.fit(x_train, df['Spam'])
 filename = 'spam_model.sav'
 
 pickle.dump(model, open(filename, 'wb'))
+
 
 def predictorspam(comment):
     simplified = remove_tags(comment)
@@ -45,4 +49,3 @@ def predictorspam(comment):
     load_model = pickle.load(open(filename, 'rb'))
     a = load_model.predict(contest)
     return a[0]
-
--- Spoken.py
+++ Spoken.py
@@ -8,47 +8,57 @@ from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import pickle
 from imblearn.over_sampling import SMOTE
-fields = ['Content','Label']
+fields = ['Content', 'Label']
 
-#Load into dataframe
-df = pd.read_csv('STdataset.csv',skipinitialspace=True,usecols=fields)
+# Load into dataframe
+df = pd.read_csv('STdataset.csv', skipinitialspace=True, usecols=fields)
 
-#Stripping function
+# Stripping function
+
+
 def remove_tags(text):
-    soup = BeautifulSoup(text,"lxml")
+    soup = BeautifulSoup(text, "lxml")
     if soup.find_all('style'):
         soup.style.decompose()
     string = soup.get_text()
-    string = string.replace('&nbsp;','').replace('\n','').replace('\r','').replace('\t','')
-    string = ' '.join([w for w in string.split() if len(w)>=3])
+    string = string.replace('&nbsp;', '').replace(
+        '\n', '').replace('\r', '').replace('\t', '')
+    string = ' '.join([w for w in string.split() if len(w) >= 3])
     return string
 
-df['Content']=df['Content'].apply(remove_tags)
 
-#Lemmatizer
+df['Content'] = df['Content'].apply(remove_tags)
+
+# Lemmatizer
+
+
 class LemmaTokenizer(object):
     def __init__(self):
         self.wnl = WordNetLemmatizer()
+
     def __call__(self, doc):
         return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
 
-#Vectorizer
-vectorizer = TfidfVectorizer(stop_words='english',tokenizer=LemmaTokenizer())
+
+# Vectorizer
+vectorizer = TfidfVectorizer(stop_words='english', tokenizer=LemmaTokenizer())
 
 x = vectorizer.fit_transform(df['Content'])
 
-#Minority oversampling
+# Minority oversampling
 sm = SMOTE(random_state=42)
 
-x,y = sm.fit_sample(x,df['Label'])
+x, y = sm.fit_sample(x, df['Label'])
 
-#Model fitting
-model = LinearSVC(random_state=42, tol=5,fit_intercept=False)
-model.fit(x,y)
+# Model fitting
+model = LinearSVC(random_state=42, tol=5, fit_intercept=False)
+model.fit(x, y)
 filename = 'tutorial_model.sav'
 pickle.dump(model, open(filename, 'wb'))
 
-#Predictor function
+# Predictor function
+
+
 def predictor(comment):
     simplified = remove_tags(comment)
     tester = [simplified]
