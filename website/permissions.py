48544d5286a36ff4f7105a7d18f5846defc35f7a
Kirti <kirti3192@gmail.com>
2021-03-26 15:37:31 +0530

    new role checking

--- website/permissions.py
+++ website/permissions.py
@@ -1,3 +1,7 @@
 def is_administrator(user):
     if user and user.groups.filter(name='Administrator').count() == 1:
         return True
+
+def is_forumsadmin(user):
+    if user and user.groups.filter(name='Forums-Admin').count() == 1:
+        return True
