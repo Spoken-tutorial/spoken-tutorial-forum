8d4672072d6fe95d88cf6491ebeb2d643629394f
abhijitbonik <abhijitbonik@hotmail.com>
2021-04-16 23:34:47 +0530

    parser args added

--- website/management/commands/fetch_questions.py
+++ website/management/commands/fetch_questions.py
@@ -7,6 +7,8 @@ from django.utils.html import strip_tags
 import uuid
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('foss', nargs='+',type=str, help='Indicates the foss list.')
 
     @tx.atomic
     def handle(self, *args, **options):
@@ -15,13 +17,15 @@ class Command(BaseCommand):
         with open(settings.MEDIA_ROOT + meta_file_name, "w+", newline='') as metafile:
             metawriter = csv.writer(metafile, dialect='excel', delimiter='|')
             metawriter.writerow(["FOSS","Tutorial","Video Minute Range","Video Second Range","Question Title","Question Body","Question_Date","Question Posted By","Answer No.","Answer Body","Answer Date", "Answer Posted By"])
-            questions = Question.objects.filter(category="Advance-C", status=1)
-            for q in questions:
-                metawriter.writerow([q.category,q.tutorial,q.minute_range,q.second_range,q.title,strip_tags(q.body).strip("&nbsp;"),q.date_created,q.user()])
-                answers = Answer.objects.filter(question=q)
-                for i, a in enumerate(answers):
-                    metawriter.writerow(["","","","","","","","",i+1, strip_tags(a.body).strip("&nbsp;"),a.date_created, a.user()])
-                metawriter.writerow([])
+            foss = options['foss']
+            for f in foss:
+                questions = Question.objects.filter(category=f, status=1)
+                for q in questions:
+                    metawriter.writerow([q.category,q.tutorial,q.minute_range,q.second_range,q.title,strip_tags(q.body).strip("&nbsp;"),q.date_created,q.user()])
+                    answers = Answer.objects.filter(question=q)
+                    for i, a in enumerate(answers):
+                        metawriter.writerow(["","","","","","","","",i+1, strip_tags(a.body).strip("&nbsp;"),a.date_created, a.user()])
+                    metawriter.writerow([])
             print("Metadata File Generated. Please find the file at location given below.")
             print(metafile.name)
 
